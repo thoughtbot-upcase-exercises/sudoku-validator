95ae757007c6e6fb9b2c283f00d1e85cb864a392
Andy Waite <github.aw@andywaite.com>
2015-04-16 21:35:37 +0100

    Extract named constant

--- lib/verify_complete.rb
+++ lib/verify_complete.rb
@@ -1,5 +1,7 @@
 class VerifyComplete
+  INCOMPLETE_SQUARE_INDICATOR = "0"
+
   def self.call(puzzle_string)
-    ! puzzle_string.include?("0")
+    ! puzzle_string.include?(INCOMPLETE_SQUARE_INDICATOR)
   end
 end
