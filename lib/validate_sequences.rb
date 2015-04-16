0fa70efc65f8333e337e2653207940d7658df3e4
Andy Waite <github.aw@andywaite.com>
2015-04-16 19:29:06 +0100

    Extract more readable methods

--- lib/validate_sequences.rb
+++ lib/validate_sequences.rb
@@ -1,7 +1,29 @@
 class ValidateSequences
-  def self.call(sequences)
+  def self.call(*args)
+    new(*args).call
+  end
+
+  def initialize(sequences)
+    self.sequences = sequences
+  end
+
+  def call
     sequences.all? do |sequence|
-      sequence.reject {|e| e == 0}.uniq == sequence.reject {|e| e == 0 }
+      constains_no_duplicates?(sequence)
     end
+  end
+
+  protected
+
+  attr_accessor :sequences
+
+  private
+
+  def constains_no_duplicates?(sequence)
+    without_zeros(sequence).uniq == without_zeros(sequence)
+  end
+
+  def without_zeros(sequence)
+    sequence.reject {|e| e == 0}
   end
 end
