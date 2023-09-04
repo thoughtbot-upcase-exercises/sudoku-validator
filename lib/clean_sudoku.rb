441fbdc8bdf8f37d1c46ff9990f66b4f1b3c16da
Andy Deng <70586367+1Dengaroo@users.noreply.github.com>
2023-09-04 15:24:32 -0400

    Update lib/clean_sudoku.rb
    Co-authored-by: Rodrigo Boniatti <boniattirodrigo@users.noreply.github.com>

--- lib/clean_sudoku.rb
+++ lib/clean_sudoku.rb
@@ -2,9 +2,7 @@ class CleanSudoku
     def self.clean_board(board)
         cleaned_board = board.split("\n").map do |row|
             next if row == "------+------+------"
-            row = row.gsub("|", "").strip.split(" ").map do |cell|
-                cell.to_i
-            end
+            row = row.gsub("|", "").strip.split(" ").map { |cell| cell.to_i }
             row
         end
         cleaned_board.compact!
